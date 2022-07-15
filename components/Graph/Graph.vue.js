90ee77872ded2d5a9ec681976aa1a7e75cb4880e
NerdyJ23 <>
2022-07-14 22:36:41 -1000

    prog

--- components/Graph/Graph.vue.js
+++ components/Graph/Graph.vue.js
@@ -1,12 +1,66 @@
 Vue.component('budget-graph', {
 	template: `
-		{{monthStruct[month]}}
+	<v-card>
+		<v-card-title>
+			{{monthStruct[month]}}
+		</v-card-title>
+		<v-card-text>
+			<canvas id="chart" class="chart"></canvas>
+		</v-card-text>
+	</v-card>
 	`
+	,inject: ["getChartJS"]
+	,mounted() {
+		this.getChartJS();
+		this.generateGraph();
+	}
+	,props: {
+		dataPoints: {
+			type: Array,
+			required:false
+		}
+	}
 	,data: function () {
 		return {
 			monthStruct: ['January','February','March','April','May','June','July','August','September','October','November','December'],
 			month: 0,
-			year: 0000
+			year: 0000,
+			config: {
+				labels: [],
+				type: 'bar',
+				animationEnabled: true,
+				axisX: {
+					title: "Month"
+				},
+				axisY: {
+					suffix: "%"
+				},
+				toolTip: {
+					shared: true,
+					reversed: true
+				},
+				legend: {
+					verticalAlign: "center",
+					horizonalAlign: "right",
+					reversed: true
+				},
+				data: []
+			}
+		}
+	}
+	,methods: {
+		generateGraph() {
+			// console.log(this.dataPoints);
+			for(let i in this.dataPoints) {
+				let item = {
+					type: "stackedColumn100",
+					name: this.dataPoints[i].name,
+				}
+				this.options.data.push(item);
+			}
+			console.log(this.options);
+			const chartLoc = document.getElementById('chart').getContext('2d');
+			const chart = new Chart(chartLoc,this.options);
 		}
 	}
 })
--- components/MainPage.vue.js
+++ components/MainPage.vue.js
@@ -1,35 +1,52 @@
 Vue.component('main-page', {
-template: `
-<div @click="getSession()">
-	<navigation ></navigation>
-</div>
-`
-,data: function() {
-	return {
-		none: null
+	template: `
+	<div>
+		<navigation></navigation>
+		<budget-graph v-if="loaded" :dataPoints="dataPoints"></budget-graph>
+	</div>
+	`
+	,mounted() {
+		this.getSession();
+		this.getChartJS();
 	}
-}
-,methods: {
-	getSession() {
-		fetch("/AJAX/BUDGET_GET_GRAPH.php", {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({month: 7, year: 2021})
-		}).then(response => {
-			console.log(response);
-			if (response.status === 200) {
-				return response.json();
-			} else {
-				throw new Error();
-			}
-		}).then(data => {
-			console.log(data);
-			console.log(data.sql);
-		}).catch(err => {
-			console.error(err);
-		})
+	,data: function() {
+		return {
+			loaded: false,
+			dataPoints: null,
+		}
 	}
-}
+	,methods: {
+		getSession() {
+			fetch("/AJAX/BUDGET_GET_GRAPH.php", {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/json'
+				},
+				body: JSON.stringify({month: 7, year: 2021})
+			}).then(response => {
+				console.log(response);
+				if (response.status === 200) {
+					return response.json();
+				} else {
+					throw new Error();
+				}
+			}).then(data => {
+				this.dataPoints = data;
+				this.loaded = true;
+			}).catch(err => {
+				console.error(err);
+				this.loaded = true;
+			})
+		}
+		,getChartJS() {
+			let chartJS = document.createElement('script')
+			chartJS.setAttribute('src', 'https://cdn.jsdelivr.net/npm/chart.js')
+			document.head.appendChild(chartJS);
+		}
+	}
+	,provide: function() {
+		return {
+			getChartJS: this.getChartJS
+		}
+	}
 })

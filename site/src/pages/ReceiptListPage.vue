770e01667203eb989e7020a377f0e898a4387168
NerdyJ23 <jessica.moolenschot@outlook.com>
2022-09-04 00:34:54 +1000

    Add seperate column for location and mobile breakpoints

--- site/src/pages/ReceiptListPage.vue
+++ site/src/pages/ReceiptListPage.vue
@@ -3,7 +3,7 @@
 <v-card class="px-4">
 	<v-card-title>
 		<v-row>
-			<v-col cols="1" class="d-flex align-center">
+			<v-col xl="1" lg="2" class="d-flex align-center">
 				<v-select
 				:items="months"
 				v-model="selectedMonth"
@@ -11,7 +11,7 @@
 				label="Month"
 				></v-select>
 			</v-col>
-			<v-col cols="1" class="d-flex align-center">
+			<v-col xl="1" lg="1" class="d-flex align-center">
 				<v-select
 				:items="years"
 				v-model="selectedYear"
@@ -19,7 +19,7 @@
 				label="Year"
 				></v-select>
 			</v-col>
-			<v-col cols="9">
+			<v-col xl="9" lg="8" class="d-none d-sm-flex">
 				<v-text-field
 				v-model="search"
 				append-icon="mdi-magnify"
@@ -31,17 +31,21 @@
 		</v-row>
 	</v-card-title>
 	<v-card-text>
+	width: {{$vuetify.breakpoint.width}}
 		<v-data-table
 		:headers="headers"
 		:search="search"
 		:items="receipts"
+		:calculateWidths="true"
 		group-by="date"
+		dense
+		mobileBreakpoint="750"
 		:loading="!loaded"
 		>
-			<template v-slot:group.header="{item, group, headers}">
-			<td :colspan="headers.length" class="primary">
-				<v-col cols="12">
-					<span class="receipt">{{group}}</span>
+			<template v-slot:group.header="{item, group, headers}" style="line-height: 1 !important">
+			<td :colspan="headers.length" class="primary" >
+				<v-col cols="12" class="d-flex">
+					<span class="receipt align-self-center">{{group}}</span>
 				</v-col>
 			</td>
 			</template>
@@ -49,7 +53,10 @@
 			<template v-slot:item="{item}">
 				<tr @click="viewReceipt(item)" class="bg">
 					<td>
-						<span>{{item.name}} - {{item.location}}</span>
+						<span>{{item.location}}</span>
+					</td>
+					<td>
+						<span>{{item.name}}</span>
 					</td>
 					<td>
 						${{item.cost.toFixed(2)}}
@@ -57,7 +64,7 @@
 					<td>
 						{{item.category}}
 					</td>
-					<td class="d-flex justify-end">
+					<td class="justify-end d-flex">
 						<div>
 							<v-btn icon @click.stop="editReceipt(item)"><v-icon color="blue lighten-2">mdi-pencil</v-icon></v-btn>
 							<v-btn icon><v-icon color="red lighten-1">mdi-delete</v-icon></v-btn>
@@ -92,8 +99,12 @@ export default {
 		return {
 			receipts: [],
 			headers: [{
-				text: 'Name',
+				text: 'Store',
 				value: 'name'
+			},
+			{
+				text: 'Location',
+				value: 'location'
 			},
 			{
 				text: 'Cost',
